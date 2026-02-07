67e041fdcd5062609084b8c77214548828c403e2
mohamed shameel m <kayalvlogger@gmail.com>
2026-02-07 18:32:01 +0530

    frontend: rebuild vite project after folder issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,35 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div style={{display:"grid",gridTemplateColumns:"3fr 1fr",height:"100vh"}}>
-      
+    <>
       <div>
-        <h2>Audit Workspace</h2>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-
-      <div>
-        <h2>AI Assistant</h2>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
       </div>
-
-    </div>
-  );
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
